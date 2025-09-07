fc08c8b29b3b02b097a25007e00b1929f7e64b5a
yenachar <yenachar@protonmail.com>
2025-09-07 05:36:24 +0200

    Added attempt at graceful shutdown first.

--- server/killold5001.sh
+++ server/killold5001.sh
@@ -1,1 +1,9 @@
-kill -9 $(lsof -t -i:5001)
+# Old way:
+# kill -9 $(lsof -t -i:5001)
+
+# New way:
+# Try graceful shutdown first
+lsof -t -i:5001 | xargs -r kill
+sleep 2
+# Force kill any remaining
+lsof -t -i:5001 | xargs -r kill -9
